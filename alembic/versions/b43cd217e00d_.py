2608d0eb7e53375d8450ec621fa74847c040dad5
agus815 <asu@fi.uba.ar>
2022-11-13 23:36:40 -0300

    fix: merge head

---
+++ alembic/versions/b43cd217e00d_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: b43cd217e00d
+Revises: a11b3a2aad64, 4e8773305ac3
+Create Date: 2022-11-14 02:31:20.536313
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b43cd217e00d'
+down_revision = ('a11b3a2aad64', '4e8773305ac3')
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
