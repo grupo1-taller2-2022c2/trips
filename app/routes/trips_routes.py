6d3104f0aa5e01dd40fc08a4dde9c6878916bee3
Agustin Gabrielli <agusgabrielli9@gmail.com>
2022-12-04 11:21:24 -0300

    Can pay trip validation when POST /trips

--- app/routes/trips_routes.py
+++ app/routes/trips_routes.py
@@ -1,8 +1,12 @@
 from app.cruds.wallets_cruds import can_pay_trip, make_payment
 from fastapi import APIRouter, Depends
 
-from app.cruds.drivers_cruds import change_driver_state, get_driver_location_by_email, save_drivers_assigned_trip_db, \
-    delete_drivers_assigned_trip_db
+from app.cruds.drivers_cruds import (
+    change_driver_state,
+    get_driver_location_by_email,
+    save_drivers_assigned_trip_db,
+    delete_drivers_assigned_trip_db,
+)
 from app.cruds.trips_cruds import *
 from sqlalchemy.orm import Session
 from starlette import status
@@ -19,7 +23,7 @@ from app.schemas.trips_schemas import TripState, TripCreate, LocationCreate
 router = APIRouter()
 
 
-url_base = os.getenv('USERS_BASE_URL')
+url_base = os.getenv("USERS_BASE_URL")
 
 
 class Pricing:
@@ -40,7 +44,9 @@ PRICING = Pricing()
 
 
 @router.get("/history/{email}", status_code=status.HTTP_200_OK)
-def get_travel_history(email: str, user_type: Union[str, None] = None, db: Session = Depends(get_db)):
+def get_travel_history(
+    email: str, user_type: Union[str, None] = None, db: Session = Depends(get_db)
+):
     if user_type == "driver":
         return get_driver_travel_history_from_db(email, db)
     return get_passenger_travel_history_from_db(email, db)
@@ -49,17 +55,19 @@ def get_travel_history(email: str, user_type: Union[str, None] = None, db: Sessi
 @router.get("/address_validation/", status_code=status.HTTP_200_OK)
 def validate_address(street_address: str, street_num: int):
     if not address_exists(street_address, street_num):
-        raise HTTPException(
-            status_code=404, detail="The location doesn't exist")
+        raise HTTPException(status_code=404, detail="The location doesn't exist")
     return
 
 
-@router.get("/saved_location/{useremail}/{location_name}", status_code=status.HTTP_200_OK)
-def get_passenger_saved_location(useremail: str, location_name: str, db: Session = Depends(get_db)):
+@router.get(
+    "/saved_location/{useremail}/{location_name}", status_code=status.HTTP_200_OK
+)
+def get_passenger_saved_location(
+    useremail: str, location_name: str, db: Session = Depends(get_db)
+):
     address_db = get_address_by_loc(useremail, location_name, db)
     if not address_db:
-        raise HTTPException(
-            status_code=404, detail="The location doesn't exist")
+        raise HTTPException(status_code=404, detail="The location doesn't exist")
     return {"street_name": address_db.street_name, "street_num": address_db.street_num}
 
 
@@ -70,17 +78,29 @@ def get_all_passenger_saved_location(useremail: str, db: Session = Depends(get_d
 
 
 @router.post("/saved_location/", status_code=status.HTTP_201_CREATED)
-def add_passenger_saved_location(location: LocationCreate, db: Session = Depends(get_db)):
+def add_passenger_saved_location(
+    location: LocationCreate, db: Session = Depends(get_db)
+):
     create_location_for_user(
-        location.email, location.location, location.street_name, location.street_num, db)
+        location.email, location.location, location.street_name, location.street_num, db
+    )
     return {"message": "Saved successfully"}
 
 
 @router.get("/cost/", status_code=status.HTTP_200_OK)
-def calculate_cost(src_address: str, src_number: int, dst_address: str, dst_number: int, passenger_email: str, duration: float,
-                   distance: float, trip_type: Union[str, None] = None, db: Session = Depends(get_db)):
-    #address_exist, address = address_exists(src_address, src_number)
-    #if not address_exist:
+def calculate_cost(
+    src_address: str,
+    src_number: int,
+    dst_address: str,
+    dst_number: int,
+    passenger_email: str,
+    duration: float,
+    distance: float,
+    trip_type: Union[str, None] = None,
+    db: Session = Depends(get_db),
+):
+    # address_exist, address = address_exists(src_address, src_number)
+    # if not address_exist:
     #    raise HTTPException(
     #        status_code=404, detail="The location isn't valid")
     price = PRICING.calculate(distance, duration)
@@ -92,8 +112,21 @@ def create_trip_and_driver_lookup(trip: TripCreate, db: Session = Depends(get_db
     url = url_base + "/drivers/all_available"
     response = requests.get(url=url)
     price = PRICING.calculate(trip.distance, trip.duration)
-    trip_db = create_trip_info(trip.src_address, trip.src_number, trip.dst_address, trip.dst_number,
-                               trip.passenger_email, price, trip.trip_type, db)
+
+    if not can_pay_trip(trip.passenger_email, price):
+        message = "Not enough balance in wallet to pay trip"
+        raise HTTPException(status_code=400, detail=message)
+
+    trip_db = create_trip_info(
+        trip.src_address,
+        trip.src_number,
+        trip.dst_address,
+        trip.dst_number,
+        trip.passenger_email,
+        price,
+        trip.trip_type,
+        db,
+    )
     if response.ok:
         distance = 0
         driver_info = None
@@ -102,21 +135,30 @@ def create_trip_and_driver_lookup(trip: TripCreate, db: Session = Depends(get_db
             if (not driver_db) or (driver_db.state == "driving"):
                 continue
             new_distance = calculate_distance(
-                trip.src_address, trip.src_number, driver_db.street_name, driver_db.street_num)
+                trip.src_address,
+                trip.src_number,
+                driver_db.street_name,
+                driver_db.street_num,
+            )
             if (new_distance < distance) or (driver_info is None):
                 distance = new_distance
                 driver_info = driver
         if not driver_info:
             return trip_db.id, None
-        send_push_message(email=driver_info["email"], title="Trip Request",
-                          message=f"You-ve received a trip request from the passenger {trip.passenger_email}.",
-                          db=db, extra={"trip_id": trip_db.id})
+        send_push_message(
+            email=driver_info["email"],
+            title="Trip Request",
+            message=f"You-ve received a trip request from the passenger {trip.passenger_email}.",
+            db=db,
+            extra={"trip_id": trip_db.id},
+        )
         assign_trip_db(trip_db.id, driver_info["email"], db)
         change_driver_state(driver_info["email"], "assigned", db)
         save_drivers_assigned_trip_db(driver_info["email"], trip_db.id, db)
         return trip_db.id, driver_info
-    raise HTTPException(status_code=response.status_code,
-                        detail=response.json()['detail'])
+    raise HTTPException(
+        status_code=response.status_code, detail=response.json()["detail"]
+    )
 
 
 @router.get("/{trip_id}", status_code=status.HTTP_200_OK)
@@ -130,38 +172,64 @@ def change_trip_state(trip: TripState, db: Session = Depends(get_db)):
         passenger_email = accept_trip_db(trip.trip_id, trip.driver_email, db)
         change_driver_state(trip.driver_email, "driving", db)
         delete_drivers_assigned_trip_db(trip.driver_email, db)
-        send_push_message(email=passenger_email, title="Trip Accepted",
-                          message="The driver has accepted the trip.", db=db)
+        send_push_message(
+            email=passenger_email,
+            title="Trip Accepted",
+            message="The driver has accepted the trip.",
+            db=db,
+        )
         return {"message": "Trip accepted"}
     if trip.status == "Deny":
         passenger_email = deny_trip_db(trip.trip_id, trip.driver_email, db)
         change_driver_state(trip.driver_email, "free", db)
         delete_drivers_assigned_trip_db(trip.driver_email, db)
-        send_push_message(email=passenger_email, title="Trip Denied",
-                          message="The driver has denied the trip.", db=db)
+        send_push_message(
+            email=passenger_email,
+            title="Trip Denied",
+            message="The driver has denied the trip.",
+            db=db,
+        )
         return {"message": "Trip denied"}
     if trip.status == "Initialize":
         driver_db = get_driver_location_by_email(trip.driver_email, db)
         trip_db = get_trip_from_id(trip.trip_id, db)
-        check_distance(driver_db.street_name, driver_db.street_num,
-                       trip_db.src_address, trip_db.src_number)
+        check_distance(
+            driver_db.street_name,
+            driver_db.street_num,
+            trip_db.src_address,
+            trip_db.src_number,
+        )
         passenger_email = initialize_trip_db(trip.trip_id, trip.driver_email, db)
         change_driver_state(trip.driver_email, "driving", db)
-        send_push_message(email=passenger_email, title="Trip Initialized",
-                          message="The driver has arrived.", db=db)
+        send_push_message(
+            email=passenger_email,
+            title="Trip Initialized",
+            message="The driver has arrived.",
+            db=db,
+        )
         return {"message": "Trip initialized"}
     if trip.status == "Finalize":
         driver_db = get_driver_location_by_email(trip.driver_email, db)
         trip_db = get_trip_from_id(trip.trip_id, db)
-        check_distance(driver_db.street_name, driver_db.street_num,
-                       trip_db.dst_address, trip_db.dst_number)
+        check_distance(
+            driver_db.street_name,
+            driver_db.street_num,
+            trip_db.dst_address,
+            trip_db.dst_number,
+        )
         passenger_email = finalize_trip_db(trip.trip_id, db)
         change_driver_state(trip.driver_email, "free", db)
-        send_push_message(email=passenger_email, title="Trip Finalized",
-                          message="The trip has finalized.", db=db)
+        send_push_message(
+            email=passenger_email,
+            title="Trip Finalized",
+            message="The trip has finalized.",
+            db=db,
+        )
         return {"message": "Trip finalized"}
     raise HTTPException(
-        status_code=400, detail="The valid status are: Accept, Deny, Initialize, Finalize")
+        status_code=400,
+        detail="The valid status are: Accept, Deny, Initialize, Finalize",
+    )
 
 
 def address_exists(street_address: str, street_num: int):
@@ -182,4 +250,5 @@ def check_distance(address_1, number_1, address_2, number_2):
     distance = calculate_distance(address_1, number_1, address_2, number_2)
     if distance > 0.1:
         raise HTTPException(
-            status_code=400, detail="The driver has not arrived to the correct place")
+            status_code=400, detail="The driver has not arrived to the correct place"
+        )
