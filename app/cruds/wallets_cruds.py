019dbccc5140d3ec04d1b5db9db00b550b5849d0
Agustin Gabrielli <agusgabrielli9@gmail.com>
2022-12-15 23:04:42 -0300

    can pay trip now checks for a little more balance than enough

--- app/cruds/wallets_cruds.py
+++ app/cruds/wallets_cruds.py
@@ -3,8 +3,8 @@ import os
 import json
 from starlette.exceptions import HTTPException
 
-wallets_base_url = os.getenv('WALLETS_BASE_URL')
-users_url_base = os.getenv('USERS_BASE_URL')
+wallets_base_url = os.getenv("WALLETS_BASE_URL")
+users_url_base = os.getenv("USERS_BASE_URL")
 
 
 def can_pay_trip(user_email, trip_price):
@@ -16,11 +16,9 @@ def can_pay_trip(user_email, trip_price):
         current_balance = float(response.json()["balance"])
         print("Current balance is: " + str(current_balance))
         print("Trip price is: " + str(trip_price))
-        print("So passenger can afford trip? " +
-              str(current_balance >= trip_price))
-        return current_balance >= trip_price
-    raise HTTPException(status_code=response.status_code,
-                        detail=response.json())
+        print("So passenger can afford trip? " + str(current_balance >= trip_price))
+        return current_balance >= trip_price * 1.05
+    raise HTTPException(status_code=response.status_code, detail=response.json())
 
 
 def make_payment(passenger_mail, driver_mail, trip_price):
@@ -29,16 +27,15 @@ def make_payment(passenger_mail, driver_mail, trip_price):
 
     url = wallets_base_url + "/transfers"
     body = {
-        'passenger_user_id': passenger_id,
-        'driver_user_id': driver_id,
-        'amount_in_ethers': format(trip_price, 'f')
+        "passenger_user_id": passenger_id,
+        "driver_user_id": driver_id,
+        "amount_in_ethers": format(trip_price, "f"),
     }
     response = requests.post(url=url, json=body)
     if response.ok:
         print("Payment made")
         return 0
-    raise HTTPException(status_code=response.status_code,
-                        detail=response.json())
+    raise HTTPException(status_code=response.status_code, detail=response.json())
 
 
 ############## AUX ##############
@@ -49,5 +46,4 @@ def get_user_id_from_email(email):
         print(response.text)
         return int(response.text)
 
-    raise HTTPException(status_code=response.status_code,
-                        detail=response.json())
+    raise HTTPException(status_code=response.status_code, detail=response.json())
